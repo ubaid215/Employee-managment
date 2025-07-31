ff0a83382eae8c5fc0f43e9a92a793426efa96bd
Muhammad Ubaidullah <ubaidtech274@gmail.com>
2025-07-31 23:20:15 +0500

    try to fix styling issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  }
 });
